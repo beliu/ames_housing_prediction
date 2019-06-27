3511a0880baa3e268a6a946026ea1bb6257f05b5
Beili Liu <nothatmakessense@gmail.com>
2019-06-27 13:05:03 -0400

    Add functions to process null values in helper.py
    Add functions that detect and extract null values in the data
    inside helper.py
    Rename numeric variables to quantitative variables to be more
    technically precise
    Format the code to be more readable

--- helper.py
+++ helper.py
@@ -8,7 +8,7 @@ def load_data(datafile, ix):
        datafile - the filename of the data
        ix - the name of the index column
 
-       RETURN:
+       RETURNS:
        data - the data from the file
     '''
 
@@ -20,26 +20,26 @@ def load_data(datafile, ix):
     return data
 
 
-def get_num_vars(data, *args):
+def get_quant_vars(data, *args):
     '''
-        Returns the variables of a dataset that are of a numeric type
+        Returns the variables of a dataset that are of quantitative
         
         ARGUMENTS:
         data - the dataframe
         args - specify additional variables that should not be
-            considered numeric
+            considered quantitative
 
-        RETURN:
-        num_vars - a dataframe containing only numeric variables
+        RETURNS:
+        num_vars - a dataframe containing only quantitative variables
     '''
     
     
     new_data = data.copy()
-    num_vars = new_data.select_dtypes(exclude=['object', 'category'])
+    quant_vars = new_data.select_dtypes(exclude=['object', 'category'])
     for arg in args:
-        num_vars = num_vars.drop(arg, axis=1)
+        quant_vars = quant_vars.drop(arg, axis=1)
 
-    return num_vars
+    return quant_vars
 
 
 def get_catg_vars(data, *args):
@@ -53,7 +53,7 @@ def get_catg_vars(data, *args):
         args - the name of any additional variables that should be 
             considered categorical
         
-        RETURN:
+        RETURNS:
         catg_vars - a dataframe containing only categorical variables
     '''
     
@@ -73,11 +73,60 @@ def conv_to_catg_type(data, catg_vars):
         data - the dataframe
         catg_vars - list of the names of the categorical variables
 
-        RETURN:
+        RETURNS:
         data - modified dataframe where the categorical variables have
             been converted to data type 'category'
     '''
-    
+
     data[catg_vars] = data[catg_vars].apply(lambda x: x.astype('category'))
     
     return data
+
+
+def get_null_vars(data):
+    '''
+        Returns the column names in a dataframe of the columns 
+        with null values. The output is separated by categorical 
+        and quantitative variables.
+
+        ARGUMENTS:
+        data - the input dataframe
+
+        RETURNS:
+        A tuple of the names of variables with null values, separated
+        by categorical and quantitative variables
+    '''
+    
+    catg_null_vars = []
+    quant_null_vars = []
+    vars_w_na_data = data.columns[data.isnull().any()].tolist()
+
+    for var in vars_w_na_data:
+        if ((data[var].dtype.name == 'category') 
+            or (data[var].dtype.name == 'object')):
+            catg_null_vars += [var]
+        else:
+            quant_null_vars += [var]
+    
+    return catg_null_vars, quant_null_vars
+
+
+def get_null_ix(data, col_names):
+    '''
+        Given a list of column names, return the indices of the rows 
+        where the null values of those columns are located.
+
+        ARGUMENTS:
+        data - the input dataframe
+        col_names - a list of the column names for which we want to find
+            the indicies of null values
+
+        RETURNS:
+        ix_dict - a dictionary whose keys are column names 
+            and whose values are a list of the indices of that column's
+            null values
+    '''
+    
+    ix_dict = {col: data[data[col].isnull()].index for col in col_names}
+    
+    return ix_dict
